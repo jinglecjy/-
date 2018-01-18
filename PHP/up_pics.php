90280352eb0269b93b56b9485270bdd6b9b466d4
jinglecjy <593775823@qq.com>
2018-01-18 22:00:47 +0800

    微博风格多图上传

--- PHP/up_pics.php
+++ PHP/up_pics.php
@@ -1,30 +1,31 @@
 <?php
 	$typeArr = array("jpg", "png", "gif"); // 允许文件上传格式
 	$path = "files/"; // 上传路径
+	if(!is_dir($path)) mkdir($path);
 	if(isset($_POST)) {
 		if($_GET["get"] == "upimg") {
 			$name = $_FILES['file']['name'];
 			$size = $_FILES['file']['size'];
 			$name_tmp = $_FILES['file']['tmp_name'];
 			if (empty($name)) {
-				echo json_encode(array("error"=>"您还未选择图片"));
+				echo json_encode(array("error"=>"您还未选择图片"), JSON_UNESCAPED_UNICODE );
 				exit;
 			}
 			$type = strtolower(substr(strrchr($name, '.'), 1)); // 获取文件类型
 			if(!in_array($type, $typeArr)) {
-				echo json_encode(array("error"=>"请上传jpg, png或gif类型的图片！"));
+				echo json_encode(array("error"=>"请上传jpg, png或gif类型的图片！"), JSON_UNESCAPED_UNICODE );
 			}
 			if($size>(1024*1024*10)) {
-				echo json_encode(array("error"=>"图片大小已超过10MB！"));
+				echo json_encode(array("error"=>"图片大小已超过10MB！"), JSON_UNESCAPED_UNICODE );
 				exit;
 			}
 
 			$pic_name = time().rand(10000, 99999).".".$type; // 重命名图片
 			$pic_url = $path.$pic_name; // 上传后的图片路径+名称
 			if(move_uploaded_file($name_tmp, $pic_url)) { // 临时文件转移到目标文件夹
-				echo json_encode(array("error"=>"0", "pic"=>$pic_url, "name"=>$pic_name));
+				echo json_encode(array("error"=>"0", "url"=>$pic_url, "name"=>$pic_name), JSON_UNESCAPED_UNICODE );
 			} else {
-				echo json_encode(array("error"=>"上传有误，请检查服务器配置！"));
+				echo json_encode(array("error"=>"上传有误，请检查服务器配置！"), JSON_UNESCAPED_UNICODE );
 			}
 		}
 	}
--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # myPlugins
 开始记录自己实现的一些小插件以便之后参考   
 
-1. **custom_check_box.html**    
+1. **index.html**   
+给我自己用的引导页！（皮这一下就很开心(\*^▽^\*)）
+2. **custom_check_box.html**    
 [用input输入框实现好看的复选框效果（css+jq）](http://blog.csdn.net/Jingle_cjy/article/details/78877397)       
 ![multi-select](http://img.blog.csdn.net/20171222223323550?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvSmluZ2xlX2NqeQ==/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast)
+3. **mul\_pics\_upload.html**   
+[仿微博风格的图片上传并实时显示实现（plupload，PHP）](http://blog.csdn.net/Jingle_cjy/article/details/79101103)   
+所需文档结构如下：   
+|- mul\_pics\_upload.html   
+|- **/ js**    
+&nbsp;&nbsp;&nbsp;|- / plupload   
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|\_ Moxie.swf   
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|\_ Moxie.xap   
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|\_ plupload.min.js   
+&nbsp;&nbsp;&nbsp;|- jquery.min.js     
+|- **/ PHP**   
+&nbsp;&nbsp;&nbsp;|\_ up_pics.php   
+&nbsp;&nbsp;&nbsp;|\_ / files   
+![多图上传](http://img.blog.csdn.net/20180118211111539?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvSmluZ2xlX2NqeQ==/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast)
